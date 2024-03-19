136b59a7581aa37b20bf23a56673fa60f2c32a17
goncaloSilva04 <goncassilva04@gmail.com>
2024-03-19 19:05:29 +0000

    Criação tabelas

--- FranciscoGoncalves_GoncaloSilva/basedados/criar_bd.sql
+++ FranciscoGoncalves_GoncaloSilva/basedados/criar_bd.sql
@@ -1,4 +1,29 @@
 --Codigo criação da BD
-
+DROP DATABASE FORMACAO_TOTAL;
 CREATE DATABASE FORMACAO_TOTAL;
 USE FORMACAO_TOTAL;
+
+CREATE TABLE utilizador(
+    username VARCHAR(40) PRIMARY KEY,
+    palavra_passe VARCHAR(40),
+    nivel   VARCHAR(40)
+);
+
+CREATE TABLE formacao(
+    nome VARCHAR(40) PRIMARY KEY,
+    num_maximo INT
+
+);
+
+CREATE TABLE inscricao(
+    username VARCHAR(40),
+    nome VARCHAR(40),
+    data_inscricao DATE,
+    FOREIGN KEY(username) REFERENCES utilizador(username),
+    FOREIGN KEY(nome) REFERENCES formacao(nome)
+);
+
+ALTER TABLE inscricao ADD CONSTRAINT PK_inscricao PRIMARY KEY(username, nome);
+
+
+
