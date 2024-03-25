2041f2364ef5d283052e72b6570e434cf922a268
Francisco-Mateus <gfrancisco@ipcbcampus.pt>
2024-03-25 20:03:55 +0000

    Estruturação da Base do Site

---
+++ FranciscoGoncalves_GoncaloSilva/paginas/criar_conta.php
@@ -0,0 +1,131 @@
+<?php
+  $username = isset($_POST["username"]) ? $_POST["username"] : "";
+  $password = isset($_POST["palavra_passe"]) ? $_POST["palavra_passe"] : "";
+  $p_confirmar = isset($_POST["confirmar_senha"]) ? $_POST["confirmar_senha"] : "";
+  $mensagem_erro = "";
+  
+  if(isset($_POST["submit"])) {
+    if($p_confirmar === $password){
+      $dbhost = 'localhost';
+      $dbuser = 'root';
+      $dbpass = '';
+
+      $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+      if(!$conn ){
+        die('Could not connect: ' . mysqli_error($conn));
+      }
+      
+      //Seleciona a base de dados
+      mysqli_select_db($conn, 'formacao_total');
+      
+      if(usernameValido($username,$conn)){
+        $sql = "INSERT INTO utilizador ( username, palavra_passe, nivel) VALUES ('".$username."', '".md5($password)."', 'cliente')";
+        $retval = mysqli_query($conn, $sql);
+        
+        if(mysqli_affected_rows($conn) == 1){//INSERT com sucesso
+          echo "<script>
+                  if(confirm('Conta criada com sucesso!')){
+                      window.location.href = 'iniciar_sessao.php';
+                  }
+              </script>";
+          exit();
+        }
+        else//INSERT falhou
+          $mensagem_erro = '<font color="red">Algo correu mal!!!</font>';
+      } 
+      $mensagem_erro = '<font color="red">Esse nome de utilizador já foi usado!</font>';
+    }
+    else
+      $mensagem_erro = '<font color="red">As palavras-passe não coincidem!</font>';
+  }
+
+  //valida se o username já está em uso
+  function usernameValido($username, $conn){
+    $sql = "SELECT * FROM utilizador WHERE username = '$username'";
+    $retval = mysqli_query($conn, $sql);
+    if(!$retval ){
+      die('Could not get data: ' . mysqli_error($conn));
+    }
+    if(mysqli_num_rows($retval) > 0)
+      return false;
+    return true;
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formação Total - Criar Conta</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Estilos personalizados -->
+  <link rel="stylesheet" href="pagina_inicial.css">
+</head>
+<body>
+  <!-- Cabeçalho -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="pagina_inicial.php">Formação Total</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="iniciar_sessao.php">Iniciar Sessão</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Criar Conta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sobre.html">Sobre</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Conteúdo da página-->
+  <br><br><br><br><br><br><br>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-md-6">
+        <div class="card" style="border: 2px solid #5a5959;">
+          <div class="card-header">
+            <h3 class="text-center">Criar Conta</h3>
+          </div>
+          <div class="card-body">
+            <center><?php echo $mensagem_erro; ?></center>
+            <form action="criar_conta.php" method="post">
+              <div class="mb-3">
+                <label for="nome" class="form-label">Nome</label>
+                <input name="nome" type="text" class="form-control" id="nome">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Nome de Utilizador</label>
+                <input type="text"  class="form-control" id="username" name="username" aria-describedby="emailHelp">
+              </div>
+              <div class="mb-3">
+                <label for="senha" class="form-label">Palavra-Passe</label>
+                <input type="password" class="form-control" id="palavra_passe" name="palavra_passe">
+              </div>
+              <div class="mb-3">
+                <label for="confirmar_senha" class="form-label">Confirmar Palavra-Passe</label>
+                <input type="password" class="form-control" id="confirmar_senha" name="confirmar_senha">
+              </div>
+              <button type="submit" name="submit" class="btn btn-primary btn-block">Criar Conta</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap JS e dependências -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ FranciscoGoncalves_GoncaloSilva/paginas/iniciar_sessao.php
@@ -0,0 +1,118 @@
+<?php
+
+  $username = isset($_POST["username"]) ? $_POST["username"] : "";
+  $password = isset($_POST["palavra_passe"]) ? $_POST["palavra_passe"] : "";
+  $mensagem_erro = "";
+  
+  if(isset($_POST["submit"])) {
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'formacao_total';
+
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    if(!$conn ){
+      die('Could not connect: ' . mysqli_error($conn));
+    }
+    
+    $sql = "SELECT nivel FROM utilizador WHERE username = '$username' AND palavra_passe = '".md5($password)."'";
+    
+    $retval = mysqli_query($conn, $sql);
+    if(!$retval ){
+      die('Could not get data: ' . mysqli_error($conn));
+    }
+
+    if(mysqli_num_rows($retval) > 0) {
+      $row = mysqli_fetch_assoc($retval);
+      if($row) {
+          // Password correcta
+          if($row['nivel']=="cliente")
+            header("Location: pagina_inicial.html");
+          else if($row['nivel']=="admin")
+            header("Location: pagina_inicial_adm.html");
+          else
+            header("Location: int_erro.html");
+          exit();
+        
+      } else {
+          //Se a $row não contiver um valor 
+          $mensagem_erro = '<font color="red">Erro ao obter dados do utilizador</font>';
+      }
+    } else {
+        //Se não encontrou o username na base de dados com a password correta
+        $mensagem_erro = '<font color="red">Credenciais incorretas</font>';
+    }
+    mysqli_close($conn);
+  }
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formação Total - Iniciar Sessão</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Estilos personalizados -->
+  <link rel="stylesheet" href="pagina_inicial.css">
+</head>
+<body>
+  <!-- Cabeçalho -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="pagina_inicial.php">Formação Total</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Iniciar Sessão</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="criar_conta.php">Criar Conta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sobre.html">Sobre</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Conteúdo da página-->
+  <br><br><br><br><br><br><br>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-md-6">
+        <div class="card" style="border: 2px solid #5a5959;">
+          <div class="card-header">
+            <h3 class="text-center">Iniciar Sessão</h3>
+          </div>
+          <div class="card-body">
+            <form method="post">
+              <div class="mb-3">
+                <label for="username" class="form-label">Nome de Utilizador</label>
+                <input type="username" name="username" class="form-control" id="email" aria-describedby="emailHelp">
+              </div>
+              <div class="mb-3">
+                <label for="palavra_passe" class="form-label">Palavra Passe</label>
+                <input type="password" name="palavra_passe" class="form-control" id="senha">
+              </div>
+              <button type="submit" name="submit" class="btn btn-primary btn-block">Iniciar Sessão</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap JS e dependências -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
